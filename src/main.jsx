9b688e7f9490ed0dd5878b2a50968362e729aa22
Mauricio da conceição silva junior <mauriciojr0705@gmail.com>
2026-05-06 14:18:11 -0300

    feat: enable cinematic camera system

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './scene-transitions.css'
+import './camera-system.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
