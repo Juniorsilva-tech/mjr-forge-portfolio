bafd6e319c70b48eba3790ebb4c987c626ef995c
Mauricio da conceição silva junior <mauriciojr0705@gmail.com>
2026-05-21 11:32:33 -0300

    Load mobile forge menu polish

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './scene-transitions.css'
 import './premium-polish.css'
+import './mobile-menu-forge.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
