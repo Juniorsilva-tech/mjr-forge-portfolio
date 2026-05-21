f2d5dd234beb07a0d708f0ad40e99830c14352bc
Mauricio da conceição silva junior <mauriciojr0705@gmail.com>
2026-05-21 16:49:33 -0300

    Enable performance menu script

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './premium-polish.css'
 import './mobile-menu-forge.css'
 import './mobile-polish.css'
 import './forge-motion-visible.css'
+import './performance-menu.js'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
